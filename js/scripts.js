fa2f8a4a376a20f6112c2bb60c33e60db62d5624
Cyril <cyril.gallitre@free.fr>
2019-06-12 18:35:06 +0200

    Modification avec jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,57 @@
-// Diaporama
-let diapos = [
-    "<div id='photo1'>Photo 1<br><img src='img/photo1.jpg'></div>",
-    "<div id='photo2'>Photo 2<br><img src='img/photo2.jpg'></div>",
-    "<div id='photo3'>Photo 3<br><img src='img/photo3.jpg'></div>"
-];
+$(document).ready(function(){
 
-let diapoEnCours = 0;
-let nbDiapos = diapos.length-1;
-let arret = false;
+    // Diaporama
+    let diapos = [
+        "<img src='img/photo1.jpg'>",
+        "<img src='img/photo2.jpg'>",
+        "<img src='img/photo3.jpg'>"
+    ];
 
-// Boucle principale : le diaporama fonctionne
-window.addEventListener("load", function(){
-    changeImage();
-}, false);
+    let diapoEnCours = 0;
+    let nbDiapos = diapos.length-1;
+    let arret = false;
 
-// Gestion du clic souris pour arrêter / relancer le diaporama
-document.getElementById("diaporama").addEventListener("click", function(){
-    if (arret===false){
-        console.log("arrêt diaporama");
-        arret = true;
-    } else {
-        console.log("reprise diaporama");
-        arret = false;
-        // on relance
-        changeImage();
+    // Gestion de la souris pour arrêter / relancer le diaporama
+    $('#diaporama').hover(
+        function() { 
+            arret = true;
+            $('#pause').fadeIn(500);
+        },
+        function() { 
+            arret = false; 
+            $('#pause').fadeOut(500);
+            changeImage();
+         }
+    );
+
+    // Gestion des touches pour contrôler le diaporama
+    document.addEventListener("keydown",function(e){
+        switch (e.keyCode) {
+            case 37: // Flèche gauche
+                diapoEnCours--;
+                document.getElementById("diaporama").innerHTML = diapos[diapoEnCours];
+                break;
+            case 39: // Flèche droite
+                diapoEnCours++;
+                document.getElementById("diaporama").innerHTML = diapos[diapoEnCours];
+                break;
+            case 27: // Echap
         }
-    }
-);
+    })
 
-// Gestion des touches pour contrôler le diaporama
-document.addEventListener("keydown",function(e){
-    switch (e.keyCode) {
-        case 37: // Flèche gauche
-            diapoEnCours = diapoEnCours-1;
-            changeImage();
-            break;
-        case 39: // Flèche droite
+    // fonction principale qui change les diapos en boucle sauf si un clic souris a été détecté (arret === false)
+    function changeImage(){
+        if (diapoEnCours > nbDiapos){
+            diapoEnCours = 0;
+        };
+
+        if (arret===false){
+            document.getElementById("diaporama").innerHTML=diapos[diapoEnCours];
             diapoEnCours++;
-            changeImage();
-            break;
-        case 27: // Echap
-    }
-})
-
-// fonction principale qui change les diapos en boucle sauf si un clic souris a été détecté (arret === false)
-function changeImage(){
-    if (diapoEnCours > nbDiapos){
-        diapoEnCours = 0;
+            setTimeout(changeImage, 2000);
+        };
     };
 
-    if (arret===false){
-        document.getElementById("diaporama").innerHTML=diapos[diapoEnCours];
-        diapoEnCours ++;
-        setTimeout(changeImage, 2000);
-    };
-};
+    // Premier lancement
+    changeImage();
+});
