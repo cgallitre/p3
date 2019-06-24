a6df35a520b96f26afc8f1faa9386fcbeeda1ccb
Cyril <cyril.gallitre@free.fr>
2019-06-24 11:52:25 +0200

    Ajout gestion de la pause et du clavier

--- js/classes.js
+++ js/classes.js
@@ -5,17 +5,58 @@ class Diaporama  {
         this.rep=rep;
         this.imageEnCours = -1;
         this.duree=duree;
+        this.boucleDiapo = 0;
+        this.cible = cible;
     }
 
-    executeDiaporama() { 
+    executeDiaporama() {
+    // Passage à la diapo suivante
         this.imageEnCours++;
+        if (this.imageEnCours >= this.tableau.length ) {
+            this.imageEnCours = 0;
+        };
         this.image.src=this.rep + this.tableau[this.imageEnCours];
-        if(this.imageEnCours==this.tableau.length-1){
-            this.imageEnCours=-1;
-        }
-        setTimeout(() => {
+ 
+        // lecture en boucle
+        this.boucleDiapo = setTimeout(() => {
             this.executeDiaporama()
-            }, this.duree);  
+            }, this.duree);
     };
-}
+
+    // Gestion de la souris et du clavier : on stoppe si au-dessus du diapo
+    executeManuel() {
+        $('#' + this.cible).hover(
+            () => {
+                clearTimeout(this.boucleDiapo);
+                this.executeClavier();
+            },
+            () => {
+                $(document).off('keyup');
+                this.executeDiaporama();
+            }
+        );
+    };
+
+    // Gestion du clavier
+    executeClavier(){
+        $(document).keyup((e) => { // action au relâchement d'une touche
+            switch (e.keyCode) {
+                case 37: // Flèche gauche
+                    this.imageEnCours--;
+                    if (this.imageEnCours ==-1) {
+                        this.imageEnCours = this.tableau.length -1;
+                    };
+                    this.image.src = this.rep + this.tableau[this.imageEnCours];
+                    break;
+                case 39: // Flèche droite
+                    this.imageEnCours++;
+                    if (this.imageEnCours == this.tableau.length) {
+                        this.imageEnCours = 0;
+                    };
+                    this.image.src = this.rep + this.tableau[this.imageEnCours];
+                    break;
+                };
+        });
+    };
+};
 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function demarreDiapo(){
     let tabDiapo = ['photo1.jpg', 'photo2.jpg', 'photo3.jpg'];
-    let diaporama = new Diaporama('diapo',tabDiapo,'img/',5000);
+    let diaporama = new Diaporama('diapo',tabDiapo,'img/',500);
     diaporama.executeDiaporama();
+    diaporama.executeManuel();
+
 }
 
 window.addEventListener("load", demarreDiapo, false);
