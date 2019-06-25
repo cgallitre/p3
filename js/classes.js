53001dad3983ac7f2c66f7c10cf61078344398fc
Cyril <cyril.gallitre@free.fr>
2019-06-25 09:35:51 +0200

    diaporama 100% fonctionnel

--- js/classes.js
+++ js/classes.js
@@ -1,86 +1,81 @@
-class Diaporama  {
-    constructor(cible,tableau,rep, duree){
-        this.image=document.getElementById(cible)
-        this.tableau=tableau;
-        this.rep=rep;
+class Diaporama {
+    constructor(cible, tableau, rep, duree) {
+        this.image = document.getElementById(cible)
+        this.tableau = tableau;
+        this.rep = rep;
         this.imageEnCours = -1;
-        this.duree=duree;
+        this.duree = duree;
         this.boucleDiapo = 0;
         this.cible = cible;
-    }
+    };
 
+    // Boucle principale du diaporama
     executeDiaporama() {
-    // Passage à la diapo suivante
-        this.imageEnCours++;
-        if (this.imageEnCours >= this.tableau.length ) {
-            this.imageEnCours = 0;
-        };
-        this.image.src=this.rep + this.tableau[this.imageEnCours];
- 
+        // Passage à la diapo suivante
+        this.diapoSuivante();
+
         // lecture en boucle
         this.boucleDiapo = setTimeout(() => {
             this.executeDiaporama()
-            }, this.duree);
+        }, this.duree);
     };
 
-    // Gestion de la souris et du clavier : on stoppe si au-dessus du diapo
-    executeManuel() {
+    // Gestion de la souris : on stoppe si au-dessus du diapo
+    executeSouris() {
+        // Passage en manuel si pointeur sur le slider
         $('#slider').hover(
             () => {
                 clearTimeout(this.boucleDiapo);
-                this.executeClavier();
-                $('#right-arrow').css('display','inline-block');
+                // this.executeClavier();
+                $('#right-arrow').css('display', 'inline-block');
                 $('#left-arrow').css('display', 'inline-block');
-                this.executeSouris();
             },
             () => {
                 $('#right-arrow').css('display', 'none');
                 $('#left-arrow').css('display', 'none');
-                $(document).off('keyup');
+                // $(document).off('keyup');
                 this.executeDiaporama();
             }
         );
+        // Gestion de la souris
+        $('#left-arrow').click(() => {
+            this.diapoPrecedente();
+        });
+
+        $('#right-arrow').click(() => {
+            this.diapoSuivante();
+        });
     };
 
     // Gestion du clavier
-    executeClavier(){
+    executeClavier() {
         $(document).keyup((e) => { // action au relâchement d'une touche
             switch (e.keyCode) {
                 case 37: // Flèche gauche
-                    this.imageEnCours--;
-                    if (this.imageEnCours ==-1) {
-                        this.imageEnCours = this.tableau.length -1;
-                    };
+                    this.diapoPrecedente();
                     break;
                 case 39: // Flèche droite
-                    this.imageEnCours++;
-                    if (this.imageEnCours == this.tableau.length) {
-                        this.imageEnCours = 0;
-                    };
+                    this.diapoSuivante();
                     break;
-                };
-                this.image.src = this.rep + this.tableau[this.imageEnCours];
+            };
         });
     };
-    // Gestion de la souris
-    executeSouris() {
-        $('#left-arrow').click(() => {
-                this.imageEnCours--;
-                if (this.imageEnCours == -1) {
-                    this.imageEnCours = this.tableau.length - 1;
-                };
-                this.image.src = this.rep + this.tableau[this.imageEnCours];
-            }
-        );
 
-        $('#right-arrow').click(() => {
-            this.imageEnCours++;
-            if (this.imageEnCours == this.tableau.length) {
-                this.imageEnCours = 0;
-            };
-            this.image.src = this.rep + this.tableau[this.imageEnCours];
-        });
+    // diapo précédente
+    diapoPrecedente() {
+        this.imageEnCours--;
+        if (this.imageEnCours === -1) {
+            this.imageEnCours = this.tableau.length - 1;
+        };
+        this.image.src = this.rep + this.tableau[this.imageEnCours];
+    };
 
+    // diapo suivante
+    diapoSuivante() {
+        this.imageEnCours++;
+        if (this.imageEnCours === this.tableau.length) {
+            this.imageEnCours = 0;
+        };
+        this.image.src = this.rep + this.tableau[this.imageEnCours];
     };
 };
-
