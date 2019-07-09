95294facd873e15e93e22388884a5aa9ae78482f
Cyril <cyril.gallitre@free.fr>
2019-07-09 15:11:04 +0200

    refactoring map.js

--- js/map.js
+++ js/map.js
@@ -1,75 +1,79 @@
 class Map {
     constructor(cible, ville, latitude, longitude) {
+        // Paramètres de la carte en entrée
         this.cible = cible;
         this.ville = ville;
         this.longitude = longitude;
         this.latitude = latitude;
+        // Définition de la carte
+        this.map = L.map(cible).setView([latitude, longitude], 13);
+        // Marqueurs
+        this.marqueur;
+        this.couleurIcon;
+        this.icon;
+        // station
+        this.mesDetails;
     };
 
     afficher() {
-        // fond de carte
-        let map = L.map(this.cible).setView([this.latitude, this.longitude], 13)
+        // fond de carte 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+        }).addTo(this.map);
 
-        // chargement des stations
-        ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=" + this.ville + "&apiKey=3a8b6dc2ac73396c5c2fb5135a07718936b00887", function (reponse) {
+        // Ajoute les stations
+        this.listingStations();
+    };
+
+    // chargement des stations
+    listingStations() {
+        ajaxGet(`https://api.jcdecaux.com/vls/v1/stations?contract=${this.ville}&apiKey=3a8b6dc2ac73396c5c2fb5135a07718936b00887`, (reponse) => {
             let stations = JSON.parse(reponse);
-            let couleurIcon;
-            for (let station of stations) {
-                // ajout des marqueurs    
-
-                // Calcul de la couleur du marqueur
-                if (station.status === "OPEN") {
-                    // la station est ouverte
-                    if (station.available_bikes > 0 && station.available_bikes < 5) {
-                        // nb vélos dispo entre 0 et 5 exclus
-                        couleurIcon = "img/yellow-dot.png";
-                    } else if (station.available_bikes === 0) {
-                        // aucun vélo dispo
-                        couleurIcon = "img/red-dot.png";
-                    } else {
-                        // nb vélos dispo > 5
-                        couleurIcon = "img/green-dot.png";
-                    }
-                } else {
-                    // station fermée
-                    couleurIcon = "img/red-dot.png"
-                };
-
-                // Définition de la couleur
-                let icon = L.icon({
-                    iconUrl: couleurIcon
-                });
-
+            for (const station of stations) {
+                // Détermine la couleur du marqueur
+                this.couleurMarqueurs(station);
                 // Affichage des marqueurs
-                let marqueur = L.marker([station.position.lat, station.position.lng], {
-                    icon: icon
-                }).addTo(map);
-
+                this.marqueur = L.marker([station.position.lat, station.position.lng], {
+                    icon: this.icon
+                }).addTo(this.map);
                 // Affichage des infos dans un popup
-                marqueur.bindPopup(station.name);
-
+                this.marqueur.bindPopup(station.name);
                 // Affichage du formulaire contextuel
-                marqueur.on('click', function () {
-                    // Affiche le détails d'une station si aucune réservation en cours
-                    if (!(sessionStorage.getItem('allowResa'))) {
-                        let mesDetails = new details(
-                            station.name,
-                            station.status,
-                            station.address,
-                            station.bike_stands,
-                            station.available_bikes,
-                            station.available_bike_stands
-                        );
-                        mesDetails.initialisation();
-                        // Cache la signature éventuellement
-                        $('#sign').hide();
-                    };
-                });
+                this.marqueur.on('click', () => this.detailsStation(station));
             };
         });
+    };
+
+    couleurMarqueurs(station) {
+        // Calcul de la couleur du marqueur
+        if (station.status === "CLOSED" || station.available_bikes === 0) {
+            this.couleurIcon = "img/red-dot.png";
+        } else if (station.available_bikes < 5) {
+            this.couleurIcon = "img/yellow-dot.png";
+        } else {
+            this.couleurIcon = "img/green-dot.png";
+        };
+
+        // Définition de la couleur
+        this.icon = L.icon({
+            iconUrl: this.couleurIcon
+        });
+    };
+
+    detailsStation(station) {
+        if (!(sessionStorage.getItem('allowResa'))) {
+            this.mesDetails = new details(
+                station.name,
+                station.status,
+                station.address,
+                station.bike_stands,
+                station.available_bikes,
+                station.available_bike_stands
+            );
+            this.mesDetails.initialisation();
+            // Cache la signature éventuellement
+            $('#sign').hide();
+        };
     };
 };
